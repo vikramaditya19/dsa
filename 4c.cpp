9e238c1633af6c957822fe21421eff9aa14f561d
vikramaditya19 <singhvikramaditya881@gmail.com>
2025-08-29 07:12:37 +0530

    Create 4c.cpp

---
+++ 4c.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter rows and columns of the matrix: ";
+    cin >> rows;
+    cin >> cols;
+
+    int matrix[rows][cols], transpose[rows][cols]; // Limit size to 50x50
+
+    cout << "Enter elements of the matrix:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    cout << "Transpose of the matrix:" << endl;
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
