b6314c76ba85eaaed48bc172dfa9e8bc770b98b0
vikramaditya19 <singhvikramaditya881@gmail.com>
2025-08-29 07:13:22 +0530

    Update 4b.cpp

--- 4b.cpp
+++ 4b.cpp
@@ -3,29 +3,50 @@
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
+    int r1, c1, r2, c2;
 
-    vector<int> arr(n);
-    cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    cout << "Enter rows and columns of first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns of second matrix: ";
+    cin >> r2 >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible. (c1 != r2)" << endl;
+        return 0;
     }
 
-    int left = 0;
-    int right = n - 1;
-    while (left < right) {
-        swap(arr[left], arr[right]);
-        left++;
-        right--;
+    int arr1[r1][c1];
+    cout << "Enter elements of first matrix:" << endl;
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> arr1[i][j];
+        }
     }
 
-    cout << "Reversed array: ";
-    for (int i=0; i<arr.size();i++) {
-        cout << arr[i] << " ";
+    int arr2[r2][c2];
+    cout << "Enter elements of second matrix:" << endl;
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> arr2[i][j];
+        }
     }
-    cout << endl;
+
+    int arr3[r1][c2];
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                arr3[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+
+    cout << "Resultant Matrix:" << endl;
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            cout << arr3[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
