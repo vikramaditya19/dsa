0ac6bc7d69327c7cd836edad222f6eec7dd8ae38
vikramaditya19 <singhvikramaditya881@gmail.com>
2025-12-04 15:22:35 +0530

    Create 1(a).cpp

---
+++ assignment4/1(a).cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+class List{
+    Node*head;
+    Node*tail;
+
+    public:
+    List(){
+        head=tail=NULL;
+    }
+    void push_front(int val){
+        Node*newNode=new Node(val);
+        if(head==NULL){
+                head=tail=newNode;
+       return;
+        }else{
+            newNode->next=head;
+            head=newNode;
+        }
+    }
+    void display(){
+        Node*temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+
+
+
+    }
+};
+int main(){
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.display();
+    return 0;
+
+}
