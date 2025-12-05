54b1e1a8538e148c4d90571fd46da17c5c62d72f
vikramaditya19 <singhvikramaditya881@gmail.com>
2025-12-05 07:44:11 +0530

    Create 3.cpp

---
+++ assignment6/3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val) {
+        data = val;
+        next = prev = NULL;
+    }
+};
+
+class DoublyList {
+    Node* head;
+
+public:
+    DoublyList() {
+        head = NULL;
+    }
+
+    void insert(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != NULL)
