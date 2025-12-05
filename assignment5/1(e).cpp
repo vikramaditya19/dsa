fe5a60e08bcac08322002e4f20a4c64de9dcb40a
vikramaditya19 <singhvikramaditya881@gmail.com>
2025-12-05 07:39:30 +0530

    Create 1(e).cpp

---
+++ assignment5/1(e).cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List {
+    Node* head;
+    Node* tail;
+
+public:
+    List() {
+        head = tail = NULL;
+    }
+
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+
+        if (head == NULL) {
+            head = tail = newNode;
+        } 
+        else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // DELETE FROM END
+    void pop_back() {
+        if (head == NULL) {
+            cout << "List is empty!";
+            return;
+        }
